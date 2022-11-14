0db3d89cac90825742309a96d31a038b1b561ab0
JJongBin <gghkdu2@gmail.com>
2022-11-14 16:40:48 +0900

    [chore] #8 emotion init
    - emotionReset 적용
    - Global css 적용
    - theme 작성

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Global } from '@emotion/react';
+import { global } from './global';
+
+function App() {
+  return (
+    <>
+      <Global styles={global} />
+    </>
+  );
+}
+
+export default App;
