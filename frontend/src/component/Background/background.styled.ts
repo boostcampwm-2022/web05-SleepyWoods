cdf0e7d36217f8d30dea2bf3caeda740fd34a2c6
ktmihs <yellowgreen423@gmail.com>
2022-11-15 15:55:22 +0900

    [design] #16 background 영상 추가 및 마크업
    - component/Background 추가

--- frontend/src/component/Background/background.styled.ts
+++ frontend/src/component/Background/background.styled.ts
@@ -1,1 +1,33 @@
 import { css } from '@emotion/react';
+import theme from '../../theme';
+
+export const header = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 20px;
+  width: 100%;
+  text-align: right;
+`;
+
+export const teamBtn = css`
+  padding: 10px 15px;
+  background-color: ${theme.white};
+  border-radius: 20px;
+  font-weight: 700;
+  z-index: 1000;
+`;
+
+export const main = css`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+`;
+
+export const video = css`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
