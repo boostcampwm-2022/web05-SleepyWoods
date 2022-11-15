46762495db96cf3b98299bab6fb814692655a03f
ktmihs <yellowgreen423@gmail.com>
2022-11-15 15:54:21 +0900

    [design] #17 modal 컴포넌트 추가
    - component/Modal 추가

--- frontend/src/component/Modal/modal.styled.ts
+++ frontend/src/component/Modal/modal.styled.ts
@@ -1,3 +1,15 @@
 import { css } from '@emotion/react';
+import theme from '../../theme';
 
-export const container = css``;
+export const container = css`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 50%;
+  min-width: 400px;
+  height: 60%;
+  min-height: 300px;
+  background: ${theme.white};
+  border-radius: 20px;
+`;
