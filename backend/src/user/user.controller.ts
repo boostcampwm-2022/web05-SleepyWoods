d02101d33738bf5e97392c6401e2151575b5c81d
comeintovlanc <rkdtjdwns916@gmail.com>
2022-11-15 19:42:42 +0900

    [feat] #4 #5 카카오 소셜 로그인 구현 (1/2)
    - kakao Oauth를 통해 유저 accessToken 받기
    - kakao Profile 조회 api 를 통해 유저 정보 받기
    - 받은 user id로 기존 유저인지 식별 후 redirect 제공

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -16,9 +16,33 @@ export class UserController {
       'http://localhost:3333/user/callback/naver' +
       '&state=' +
       'RANDOM_STATE';
+
+    const kakaoOauthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_REST_API_KEY}&redirect_uri=http://localhost:3333/user/callback/kakao&response_type=code`;
     switch (social) {
       case socialPlatform.NAVER:
         res.redirect(naverOauthUrl);
+        break;
+      case socialPlatform.KAKAO:
+        res.redirect(kakaoOauthUrl);
+        break;
+    }
+  }
+
+  @Get('callback/kakao')
+  async kakaoCallback(@Query('code') code: string, @Res() res: Response) {
+    const access_token = await this.userService.kakaoOauth(code);
+    const kakaoProfile = await this.userService.kakaoProfileSearch(
+      access_token
+    );
+    console.log(kakaoProfile);
+    const userData = await this.userService.findUser(
+      socialPlatform.KAKAO,
+      kakaoProfile.id
+    );
+    if (userData == null) {
+      return res.redirect('http://localhost:3333/user'); // 가입으로 보내요
+    } else {
+      return res.redirect('http://naver.com'); // 쿠기 생성해서 메인으로 보내요
     }
   }
 
