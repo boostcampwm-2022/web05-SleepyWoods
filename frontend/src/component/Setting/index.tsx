d5823977439ceb53e39c7944441f45ff206346a7
JJongBin <gghkdu2@gmail.com>
2022-11-16 11:47:25 +0900

    [feat] Signin component 개발
    - 마크업 및 스타일링
    - 헤어 선택(useState)

--- frontend/src/component/Setting/index.tsx
+++ frontend/src/component/Setting/index.tsx
@@ -1,5 +1,54 @@
-const Setting = () => {
-  return;
+import { useState } from 'react';
+import {
+  header,
+  character,
+  nicknameContainer,
+  nickname,
+  signupBtn,
+  prevBtn,
+  nextBtn,
+  CarowselContainer,
+  characterWrapper,
+} from './setting.styled';
+import hair from './hair';
+
+const Carowsel = () => {
+  const [hairIdx, setHairIdx] = useState(0);
+
+  const minusIdx = () => {
+    if (hairIdx - 1 < 0) setHairIdx(hair.length);
+    else setHairIdx(hairIdx - 1);
+  };
+
+  const plusIdx = () => {
+    if (hairIdx + 1 > hair.length) setHairIdx(0);
+    else setHairIdx(hairIdx + 1);
+  };
+
+  return (
+    <div css={CarowselContainer}>
+      <button type="button" css={prevBtn} onClick={minusIdx}></button>
+      <div css={characterWrapper}>
+        <div css={character(hair[hairIdx])}></div>
+      </div>
+      <button type="button" css={nextBtn} onClick={plusIdx}></button>
+    </div>
+  );
 };
+
+const Setting = () => (
+  <>
+    <h2 css={header}>Setting</h2>
+    <Carowsel />
+    <div css={nicknameContainer}>
+      <input
+        type="text"
+        css={nickname}
+        placeholder="설정할 닉네임을 입력하세요."
+      />
+      <button css={signupBtn}>Signup</button>
+    </div>
+  </>
+);
 
 export default Setting;
