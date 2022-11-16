e765809f33e419ec0956ceb03a067d2c8ede7026
JJongBin <gghkdu2@gmail.com>
2022-11-16 10:20:35 +0900

    [refactor] signin page에서 Setting component 분리로 인한 코드 수정

--- frontend/src/page/Signup/index.tsx
+++ frontend/src/page/Signup/index.tsx
@@ -1,19 +1,12 @@
 import Background from '../../component/Background';
 import Modal from '../../component/Modal';
-import { header, avatars, nickname, signupBtn } from './signup.styled';
+import Setting from '../../component/Setting';
 
 const Signup = () => {
   return (
     <Background>
       <Modal>
-        <h2 css={header}>Setting</h2>
-        <div css={avatars}>1</div>
-        <input
-          type="text"
-          css={nickname}
-          placeholder="설정할 닉네임을 입력하세요."
-        />
-        <button css={signupBtn}>Signup</button>
+        <Setting />
       </Modal>
     </Background>
   );
