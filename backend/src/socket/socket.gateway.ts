18d96fe0728b7b5b2d765c9f57d386424d6efd20
comeintovlanc <rkdtjdwns916@gmail.com>
2022-11-28 16:51:23 +0900

    [feat] 소켓 입장 로직 완료
    - userCreated 이벤트로 다른 유저들에게 입장한 유저 정보 배열로 전달
    - userCreated 이벤트로 입장한 유저에게 다른 유저들 전체 정보 전달

--- backend/src/socket/socket.gateway.ts
+++ backend/src/socket/socket.gateway.ts
@@ -8,6 +8,7 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { AuthService } from 'src/auth/auth.service';
+import { UserDataDto } from 'src/auth/dto/user-data.dto';
 
 @WebSocketGateway()
 export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -17,16 +18,35 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(private readonly authService: AuthService) {}
 
   // 입장 햇을 시
-  // 이사람이 누군지 먼저 판별?
 
+  public getRoomUserData(roomName) {
+    const roomUser = [];
+    this.server.sockets.adapter.rooms.get(roomName).forEach(e => {
+      roomUser.push(this.server.sockets.sockets.get(e)['userData']);
+    });
+
+    return roomUser;
+  }
   public handleConnection(client: Socket): void {
     const key = client.handshake.headers.authorization;
     const userData = this.authService.verify(key);
     if (!userData) {
       client.disconnect();
+      return;
     }
 
-    console.log('connected', client.id);
+    client['userData'] = {
+      ...userData,
+      x: -25,
+      y: 400,
+      socketId: client.id,
+      direction: 'right',
+      state: 'walk',
+    };
+
+    client.join('chat');
+    client.to('chat').emit('userCreated', client['userData']);
+    this.server.to(client.id).emit('userCreated', this.getRoomUserData('chat'));
   }
 
   // 퇴장 햇을 시
