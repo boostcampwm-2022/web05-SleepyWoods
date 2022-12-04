d93f99905281040908cafb06f7b968f0f5967b7b
JJongBin <gghkdu2@gmail.com>
2022-12-04 17:12:56 +0900

    [fix] #126 뒤로가기 후 재실행시 움직이지 않는 에러
    - phaser.game의 인스턴스가 남아있던 것으로 추측 -> destroy(true)로 해결

--- frontend/src/component/Game/index.tsx
+++ frontend/src/component/Game/index.tsx
@@ -2,6 +2,7 @@ import { useRef, useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 import { socketState } from '../../store/atom/socket';
 import { userState } from '../../store/atom/user';
+import gameConfig from './gameConfig';
 import config from './gameConfig';
 import { emitter } from './util';
 
@@ -17,9 +18,15 @@ const Game = () => {
       emitter.emit('init', { ...user, socket });
     });
 
-    if (ref.current) {
-      new Phaser.Game({ ...config, parent: ref.current });
-    }
+    if (!ref.current) return;
+    const game = new Phaser.Game({ ...config, parent: ref.current });
+
+    return () => {
+      emitter.removeAllListeners();
+      socket.removeAllListeners();
+      socket.disconnect();
+      game.destroy(true);
+    };
   }, [user]);
 
   return <div ref={ref}></div>;
