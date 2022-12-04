158e25fa37c89b6b1184c4c586bdbd86c8f9c40e
ktmihs <yellowgreen423@gmail.com>
2022-12-04 17:04:31 +0900

    [fix] #129 uuid 호출
    - 변수로 잘못 사용된 uuid를 함수 호출로 변경

--- frontend/src/store/atom/backgroundSetting.ts
+++ frontend/src/store/atom/backgroundSetting.ts
@@ -2,11 +2,11 @@ import { atom } from 'recoil';
 import { v1 } from 'uuid';
 
 export const musicState = atom<boolean>({
-  key: `musicState/${v1}`,
+  key: `musicState/${v1()}`,
   default: false,
 });
 
 export const snowState = atom<boolean>({
-  key: `snowState/${v1}`,
+  key: `snowState/${v1()}`,
   default: true,
 });
