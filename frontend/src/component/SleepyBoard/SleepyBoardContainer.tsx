05aebc6cdb4810ad58cbced06306812af7e67307
JJongBin <gghkdu2@gmail.com>
2022-12-05 00:17:33 +0900

    [feat] #134 SleepyBoard 및 SleepyRank 구현
    - 탭 사용(지역 상태 관리)
    - 탭 상태에 따른 랜더링
    - 순위 페이지 1~3위 구분
    - 최소 너비 및 높이 조정
    - 랭킹 api 미적용

--- frontend/src/component/SleepyBoard/SleepyBoardContainer.tsx
+++ frontend/src/component/SleepyBoard/SleepyBoardContainer.tsx
@@ -1,12 +1,5 @@
 import { useEffect, useState } from 'react';
-import {
-  modal,
-  header,
-  title,
-  contentWrapper,
-  filterBtn,
-  filterBtnBox,
-} from './sleepyboard.styled';
+import * as style from './sleepyboard.styled';
 import Content from './content';
 import like from '../../assets/icon/like.svg';
 import user from '../../assets/icon/user.svg';
@@ -20,7 +13,6 @@ const SleepyBoardContainer = ({ animation }: { animation: string }) => {
   }, []);
 
   const getPost = async (key: string) => {
-    // API 요청
     try {
       const { data, status } = await axios(`/api/board/${key}`);
       if (status === 200) setBoard(data);
@@ -28,26 +20,29 @@ const SleepyBoardContainer = ({ animation }: { animation: string }) => {
   };
 
   return (
-    <section css={modal(animation)}>
-      <header css={header}>
-        <h2 css={title}>SleepyBoard</h2>
-        <div css={filterBtnBox}>
-          <button
-            type="button"
-            onClick={() => getPost('like')}
-            css={filterBtn(like, 22, 18)}></button>
-          <button
-            type="button"
-            onClick={() => getPost('my')}
-            css={filterBtn(user, 20, 20)}></button>
-        </div>
-      </header>
-      <ul css={contentWrapper}>
+    <>
+      <nav css={style.filterBtnBox}>
+        <button
+          type="button"
+          onClick={() => getPost('all')}
+          css={style.filterBtn('', 0, 0)}>
+          All
+        </button>
+        <button
+          type="button"
+          onClick={() => getPost('like')}
+          css={style.filterBtn(like, 22, 18)}></button>
+        <button
+          type="button"
+          onClick={() => getPost('my')}
+          css={style.filterBtn(user, 20, 20)}></button>
+      </nav>
+      <ul css={style.contentWrapper}>
         {board.map((data: any) => (
           <Content key={data.id} data={data} />
         ))}
       </ul>
-    </section>
+    </>
   );
 };
 export default SleepyBoardContainer;
