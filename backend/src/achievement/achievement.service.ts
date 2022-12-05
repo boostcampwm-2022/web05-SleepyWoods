c03ba5580401eabcb8a9adc18a598863cb538f83
comeintovlanc <rkdtjdwns916@gmail.com>
2022-12-05 18:41:25 +0900

    [feat] #139 걸음 수 통계 API 지원
    - GET /achievement/walk?year={}&month={}&range={'all' | 'friend'} 를 통해 통계 API

--- backend/src/achievement/achievement.service.ts
+++ backend/src/achievement/achievement.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Walk } from './entity/walk.entity';
@@ -9,13 +9,51 @@ export class AchievementService {
     @InjectRepository(Walk) private walkRepository: Repository<Walk>
   ) {}
 
+  async getUsersWalkCounts(
+    year: number,
+    month: number,
+    range: string,
+    userList: any
+  ) {
+    try {
+      const result = await this.walkRepository
+        .createQueryBuilder('walk')
+        .select([
+          'walk.userid as userid',
+          'walk.year as year',
+          'walk.month as month',
+          'SUM(walk.walk) as walkcount ',
+          'user.nickname as nickname',
+        ])
+        .innerJoin('walk.user', 'user')
+        .groupBy('walk.userid, walk.year, walk.month, user.nickname')
+        .having(
+          range == 'all'
+            ? 'walk.year = :year AND walk.month = :month'
+            : 'walk.year = :year AND walk.month = :month AND walk.userid in (:...userList)',
+          { year, month, userList: userList.map(user => user.userId) }
+        )
+        .orderBy('SUM(walk.walk)', 'DESC')
+        .getRawMany();
+      return result;
+    } catch (e) {
+      return [];
+    }
+  }
+
   async saveWalkCount(walkCountByUser: any) {
     try {
-      const today = new Date().toISOString().substring(0, 10);
+      const today = new Date();
+      const year = today.getFullYear();
+      const month = today.getMonth() + 1;
+      const day = today.getDate();
+
       const walkCountArray = walkCountByUser.map(([userId, walk]) => {
         return {
           userid: userId,
-          date: today,
+          year,
+          month,
+          day,
           walk: Math.floor(walk / 10),
         };
       });
