c9903ca54300efb43f8295f8ac09b4b0b448c280
JJongBin <gghkdu2@gmail.com>
2022-12-06 15:11:30 +0900

    [feat] #3 채팅 메세지 시간 계산 및 자신이 보낸 메세지인지 확인

--- frontend/src/component/Sidebar/Chat/ChatMessage.tsx
+++ frontend/src/component/Sidebar/Chat/ChatMessage.tsx
@@ -1,18 +1,16 @@
 import { useRecoilValue } from 'recoil';
 import { userState } from '../../../store/atom/user';
 import * as style from './chat.styled';
+import { calcTimeFromMs } from './util';
 
 const ChatMessage = ({ chat }: { chat: any }) => {
-  // const user = useRecoilValue(userState);
-  // const isSender = user.nickname === chat.nickname;
-
-  const isSender =
-    'Z-NVwpPpbw7oOjHEn-CYEZ8V9nU3P9IIChz_9PuuVak' === chat.senderId;
+  const user = useRecoilValue(userState);
+  const isSender = user.id === chat.senderId;
 
   return (
     <li css={style.chatItem(isSender)}>
       <div css={style.chatText(isSender)}>{chat.message}</div>
-      <div css={style.chatTime}>{chat.timestamp}</div>
+      <div css={style.chatTime}>{calcTimeFromMs(chat.timestamp)}</div>
     </li>
   );
 };
