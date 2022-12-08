813392cd88b48685b695976bbb48cc7ee0a51b36
hj1n <hjin8@korea.ac.kr>
2022-12-08 19:03:59 +0900

    [feat] #164 #165 유저상태 및 통화 관련 소켓이벤트

--- backend/src/socket/socket.gateway.ts
+++ backend/src/socket/socket.gateway.ts
@@ -170,6 +170,107 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
     });
   }
 
+  @SubscribeMessage('callRequested')
+  handleCallRequested(client: any, payload: any) {
+    const { calleeUserId } = payload;
+    const callerUserId = client['userData']['id'];
+    // busy 로 상태 변경
+    client['userData']['userState'] = 'busy';
+    this.server.sockets.sockets.get(this.socketIdByUser.get(calleeUserId))[
+      'userData'
+    ]['userState'] = 'busy';
+
+    this.server
+      .to(this.socketIdByUser.get(calleeUserId))
+      .emit('callRequested', {
+        callerUserId: client['userData']['id'],
+      });
+
+    this.server.to(client['userData']['roomName']).emit('userStateChanged', {
+      userIdList: [calleeUserId, callerUserId],
+      userState: 'busy',
+    });
+  }
+
+  // 건 사람이 마음 바뀜 끊음. => 상대방만 on으로 변경
+  @SubscribeMessage('callCanceled')
+  handleCallCanceled(client: any, payload: any) {
+    const { calleeUserId } = payload;
+    const callerUserId = client['userData']['id'];
+
+    this.server.sockets.sockets.get(this.socketIdByUser.get(calleeUserId))[
+      'userData'
+    ]['userState'] = 'on';
+
+    this.server.to(this.socketIdByUser.get(calleeUserId)).emit('callCanceled', {
+      callerUserId: client['userData']['id'],
+    });
+
+    this.server.to(client['userData']['roomName']).emit('userStateChanged', {
+      userIdList: [calleeUserId],
+      userState: 'on',
+    });
+  }
+
+  @SubscribeMessage('callDenied')
+  handleCallDenied(client: any, payload: any) {
+    const { callerUserId } = payload;
+    const calleeUserId = client['userData']['id'];
+
+    client['userData']['userState'] = 'on';
+
+    this.server.to(this.socketIdByUser.get(callerUserId)).emit('callDenied', {
+      calleeUserId: client['userData']['id'],
+    });
+
+    this.server.to(client['userData']['roomName']).emit('userStateChanged', {
+      userIdList: [calleeUserId],
+      userState: 'on',
+    });
+  }
+
+  // 전화 수락!!
+  @SubscribeMessage('callEntered')
+  handleCallEntered(client: any, payload: any) {
+    const { callerUserId } = payload;
+    const calleeUserId = client['userData']['id'];
+
+    client['userData']['userState'] = 'busy';
+    this.server.sockets.sockets.get(this.socketIdByUser.get(calleeUserId))[
+      'userData'
+    ]['userState'] = 'busy';
+
+    this.server.to(this.socketIdByUser.get(callerUserId)).emit('callEntered', {
+      calleeUserId: client['userData']['id'],
+    });
+
+    this.server.to(client['userData']['roomName']).emit('userStateChanged', {
+      userIdList: [callerUserId, calleeUserId],
+      userState: 'busy',
+    });
+    // webRTC 연결 맺애주는 과정 필요
+    // 기존에 그룹에 통화하고잇던 리스트를 받아서 userData어딘가에 갖고있어야할듯?
+    // 내가 아무도없는 상태에서 너한테 거는거랑 ( 진짜로 webRTC룸이 첫 생성)
+    // 내가 누군가랑 하다가 너까지 끌어들이는 시점 (룸이 잇는데 너를 끌어드림)
+    // 프론트에서 저장하고잇다가 request할떄 같이 참여자들 리스트?
+  }
+  // 전화 나오기!
+
+  @SubscribeMessage('callLeaved')
+  handleCallLeaved(client: any, payload: any) {
+    const leavingUserId = client['userData']['id'];
+    const { callingUserList } = payload;
+
+    this.server.to(callingUserList).emit('callLeaved', {
+      leavingUserId,
+    });
+
+    this.server.to(client['userData']['roomName']).emit('userStateChanged', {
+      userIdList: [leavingUserId],
+      userState: 'on',
+    });
+  }
+
   @SubscribeMessage('disconnecting')
   handleDisconnecting(client: any) {
     client.disconnect();
