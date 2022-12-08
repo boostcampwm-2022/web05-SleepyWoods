b9a5043f571483e1f398c3b3783267e59a1e8ed2
ktmihs <yellowgreen423@gmail.com>
2022-12-08 20:12:34 +0900

    [fix] #49 로컬 환경에서의 소켓 주소 수정
    - 포트 번호가 프론트로 적용되어있던 것을 서버 단으로 변경

--- frontend/src/store/atom/socket.ts
+++ frontend/src/store/atom/socket.ts
@@ -3,9 +3,12 @@ import { io, Socket } from 'socket.io-client';
 import { getCookieValue } from '../../page/Main';
 import { v1 } from 'uuid';
 
+const path =
+  location.origin === 'sleepywoods.kr' ? 'sleepywoods.kr' : 'localhost:3333';
+
 export const socketState = atom<Socket>({
   key: `socketState/${v1()}`,
-  default: io(location.origin, {
+  default: io(path, {
     autoConnect: false,
     extraHeaders: {
       authorization: getCookieValue('accessToken'),
